218bfcf5e1b6a76963a74b4139a45ae8cd727fbe
Derrick Ellerbie <dellerbie@gmail.com>
2018-03-24 10:58:35 -0400

    Added spec for scoping FinancialSummary to a given user

---
+++ spec/models/financial_summary/summarizes_only_for_given_user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe FinancialSummary do
+  it 'summarizes only for the user given' do
+    derrick = create(:user)
+    kim = create(:user)
+
+    [derrick, kim].each do |user|
+      create(:transaction, user: user, category: :deposit)
+      create(:transaction, user: user, category: :withdraw)
+    end
+
+    summary = FinancialSummary.one_day(user: derrick, currency: :usd)
+    expect(summary.count(:deposit)).to eq(1)
+    expect(summary.amount(:deposit)).to eq(Money.from_amount(1, :usd))
+    expect(summary.count(:withdraw)).to eq(1)
+    expect(summary.amount(:withdraw)).to eq(Money.from_amount(1, :usd))
+  end
+end
--- spec/models/financial_summary/summarizes_over_timeframe_spec.rb
+++ spec/models/financial_summary/summarizes_over_timeframe_spec.rb
@@ -23,39 +23,39 @@ describe FinancialSummary do
   it 'summarizes over one day' do
     user = create_user_with_transactions_on(time_in: Time.current, time_out: 2.days.ago)
 
-    subject = FinancialSummary.one_day(user: user, currency: :usd)
+    summary = FinancialSummary.one_day(user: user, currency: :usd)
 
-    expect(subject.count(:deposit)).to eq(2)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
-    expect(subject.count(:withdraw)).to eq(1)
-    expect(subject.amount(:withdraw)).to eq(Money.from_amount(0.12, :usd))
-    expect(subject.count(:refund)).to eq(1)
-    expect(subject.amount(:refund)).to eq(Money.from_amount(5, :usd))
+    expect(summary.count(:deposit)).to eq(2)
+    expect(summary.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
+    expect(summary.count(:withdraw)).to eq(1)
+    expect(summary.amount(:withdraw)).to eq(Money.from_amount(0.12, :usd))
+    expect(summary.count(:refund)).to eq(1)
+    expect(summary.amount(:refund)).to eq(Money.from_amount(5, :usd))
   end
 
   it 'summarizes over seven days' do
     user = create_user_with_transactions_on(time_in: 5.days.ago, time_out: 8.days.ago)
 
-    subject = FinancialSummary.seven_days(user: user, currency: :usd)
+    summary = FinancialSummary.seven_days(user: user, currency: :usd)
 
-    expect(subject.count(:deposit)).to eq(2)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
-    expect(subject.count(:withdraw)).to eq(1)
-    expect(subject.amount(:withdraw)).to eq(Money.from_amount(0.12, :usd))
-    expect(subject.count(:refund)).to eq(1)
-    expect(subject.amount(:refund)).to eq(Money.from_amount(5, :usd))
+    expect(summary.count(:deposit)).to eq(2)
+    expect(summary.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
+    expect(summary.count(:withdraw)).to eq(1)
+    expect(summary.amount(:withdraw)).to eq(Money.from_amount(0.12, :usd))
+    expect(summary.count(:refund)).to eq(1)
+    expect(summary.amount(:refund)).to eq(Money.from_amount(5, :usd))
   end
 
   it 'summarizes over lifetime' do
     user = create_user_with_transactions_on(time_in: 8.days.ago, time_out: 30.days.ago)
 
-    subject = FinancialSummary.lifetime(user: user, currency: :usd)
+    summary = FinancialSummary.lifetime(user: user, currency: :usd)
 
-    expect(subject.count(:deposit)).to eq(3)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(13.12, :usd))
-    expect(subject.count(:withdraw)).to eq(2)
-    expect(subject.amount(:withdraw)).to eq(Money.from_amount(1.12, :usd))
-    expect(subject.count(:refund)).to eq(2)
-    expect(subject.amount(:refund)).to eq(Money.from_amount(6, :usd))
+    expect(summary.count(:deposit)).to eq(3)
+    expect(summary.amount(:deposit)).to eq(Money.from_amount(13.12, :usd))
+    expect(summary.count(:withdraw)).to eq(2)
+    expect(summary.amount(:withdraw)).to eq(Money.from_amount(1.12, :usd))
+    expect(summary.count(:refund)).to eq(2)
+    expect(summary.amount(:refund)).to eq(Money.from_amount(6, :usd))
   end
 end
