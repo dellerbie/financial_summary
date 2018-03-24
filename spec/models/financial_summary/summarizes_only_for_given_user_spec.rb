df8ee2d4796d5e26285657adb095dbf0cf9d43e9
Derrick Ellerbie <dellerbie@gmail.com>
2018-03-24 11:19:53 -0400

    Clean up specs formatting

--- spec/models/financial_summary/summarizes_only_for_given_user_spec.rb
+++ spec/models/financial_summary/summarizes_only_for_given_user_spec.rb
@@ -4,13 +4,13 @@ describe FinancialSummary do
   it 'summarizes only for the given user' do
     derrick = create(:user)
     kim = create(:user)
-
     [derrick, kim].each do |user|
       create(:transaction, user: user, category: :deposit)
       create(:transaction, user: user, category: :withdraw)
     end
 
     summary = FinancialSummary.one_day(user: derrick, currency: :usd)
+
     expect(summary.count(:deposit)).to eq(1)
     expect(summary.amount(:deposit)).to eq(Money.from_amount(1, :usd))
     expect(summary.count(:withdraw)).to eq(1)
